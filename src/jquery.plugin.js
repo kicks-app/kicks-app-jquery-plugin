1eeeee01e8cf5520f3680a022a5313af024d85e5
Rafael Nowrotek <mail@benignware.com>
2016-02-24 22:01:20 +0100

    Implemented method logic and added sample logic

--- src/jquery.plugin.js
+++ src/jquery.plugin.js
@@ -19,9 +19,7 @@
     // Init plugin instance
     var
       $element = $(element),
-      opts = $.extend(true, {}, options, {
-        // Plugin Defaults:
-      }, $element.data());
+      opts = {};
     
     /**
      * Updates the component
@@ -29,16 +27,43 @@
      */
     this.update = function(options) {
       $.extend(opts, options);
+      // Update logic
+      $element.html(opts.foo);
     };
     
+    /**
+     * Example method
+     * @param {String} bar
+     */
+    this.foo = function(bar) {
+      $.extend(opts, options);
+      // Method logic
+      $element.html(bar || opts.foo);
+    };
+
+    
     // Initial update
-    this.update();
+    this.update($.extend(true, {
+      // Plugin Defaults:
+      foo: 'Bar'
+    }, options, $element.data()));
   }
   
   // Add Plugin to registry
-  $.fn.plugin = function(options) {
+  $.fn.plugin = function() {
+    var
+      args = [].slice.call(arguments);
     return this.each(function() {
-      return ($(this).data('plugin') && $(this).data('plugin').update(options) || $(this).data('plugin', new Plugin(this, options)));
+      return (function(instance) {
+        var
+          result;
+        // Update or init plugin
+        $(this).data('plugin', instance = instance ? typeof args[0] === 'object' && instance.update(args[0]) && instance || instance : new Plugin(this, args[0]));
+        // Call method
+        result = typeof args[0] === 'string' && typeof instance[args[0]] === 'function' ? instance[args[0]].apply(instance, args.slice(1)) : result;
+        // Return undefined or chaining element
+        return typeof result !== 'undefined' ? result : this;
+      }).call(this, $(this).data('plugin'));
     });
   };
 
